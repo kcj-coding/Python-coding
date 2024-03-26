7e5820e61358dfe1c2ea1e61cdd0e858c7cd2f4e
Kelvin Chilvers-Jones <119540748+kcj-coding@users.noreply.github.com>
2024-03-26 15:04:36 +0000

    Add files via upload

--- strings.py
+++ strings.py
@@ -12,7 +12,9 @@ ltrs = re.sub("([^a-zA-Z])+", " ", string)
 nbrs = re.sub("([^0-9])+", " ", string)
 
 # make punctuation only
-punc = re.sub("[[:punct:]]", " ", string)
+#punc = re.compile("[[:punct:]]")
+punc = re.compile("[!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]")
+punc = punc.findall(string)
 
 # make numbers and letters only
 nbrs_ltrs = re.sub("([^0-9a-zA-Z])+", " ", string)
@@ -29,3 +31,18 @@ split_string = str.split(string, ".")
 # capitalise only the first word
 #cap = str.title(string)
 cap = '. '.join(list(map(lambda x: x.strip().capitalize(), string.split('.'))))
+
+################################################################################
+
+string1 = "The.Day//Was1111111Good for the 10 of the month being 10/03/2024 a Sunday"
+
+# extract everything after the //
+ext = re.sub("\\W\\W+", " ", string1)
+
+# extract date only
+date = re.compile("\\d+/\\d+/\\d+")
+date = date.findall(string1)
+
+# get numbers not joined to any words
+std_nm = re.compile("\\s\\d+\\s")
+std_nm = std_nm.findall(string1)
